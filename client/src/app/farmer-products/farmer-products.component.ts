57b92c48eaea5bb56d8fd6138692894eede2102f
Sam Mousa <sammousa@Sams-MacBook-Pro.local>
2020-08-09 22:13:55 -0500

    full front end farmer

--- client/src/app/farmer-products/farmer-products.component.ts
+++ client/src/app/farmer-products/farmer-products.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FarmersService } from './../services/farmers.service'
 
 @Component({
   selector: 'app-farmer-products',
@@ -6,10 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./farmer-products.component.css']
 })
 export class FarmerProductsComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  products;
+  constructor(private Farmers: FarmersService) { 
+    this.Farmers.getProducts();
   }
-
+  ngOnInit(){
+    
+      this.Farmers.subject.subscribe(products => {
+        if (!this.products) {
+          this.products = products;
+          console.log(this.Farmers.getProducts());
+        }
+      });
+  }
+  increaseQuantity(index){
+    console.log(this.products[index]._id)
+    const id = this.products[index]._id
+    this.Farmers.increaseProductQuantity(id);
+  }
+  decreaseQuantity(index){
+    console.log(this.products[index]._id)
+    const id = this.products[index]._id
+    this.Farmers.decreseProductQuantity(id);
+  }
+  deleteProduct(index){
+    console.log(this.products[index]._id)
+    const id = this.products[index]._id
+    this.Farmers.removeProduct(id);
+  }
+  
 }
--- client/src/app/farmer/farmer.component.ts
+++ client/src/app/farmer/farmer.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { FarmersService } from './../services/farmers.service'
 
 @Component({
   selector: 'app-farmer',
   templateUrl: './farmer.component.html',
   styleUrls: ['./farmer.component.css']
 })
-export class FarmerComponent implements OnInit {
+export class FarmerComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor(private Farmers: FarmersService) { }
+  
+ 
 }
